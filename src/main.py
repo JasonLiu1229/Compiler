8d5b9ca61b8c90e3d388ce22f81e0fb9522191f4
Orfeo Terkuci <terkuciorfeo@gmail.com>
2023-02-20 21:49:18 +0100

    Basic test code to parse the grammar from a file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import sys
 from antlr4 import *
+from Grammars.output.MathLexer import MathLexer
+from Grammars.output.MathParser import MathParser
 
 def main(argv):
     input_stream = FileStream(argv[1])
-
+    lexer = MathLexer(input_stream)
+    parser = MathParser(CommonTokenStream(lexer))
+    parse_tree = parser.math()
+    print(parse_tree.toStringTree(recog=parser))
 
 if __name__ == '__main__':
     main(sys.argv)
