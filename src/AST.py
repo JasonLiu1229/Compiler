c13fda25cfcbb53352327fd7bc3d16e97946946d
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-02-21 14:56:24 +0100

    ast + nodes

--- src/AST.py
+++ src/AST.py
@@ -1,14 +1,37 @@
-class treeNode:
+class Node:
     def __init__(self, token, value):
         self.token = token
         self.value = value
 
+    class exprNode:
+        def __init__(self) -> None:
+            super().__init__()
+
+    class bin_opNode:
+        def __init__(self) -> None:
+            super().__init__()
+
+    class un_opNode:
+        def __init__(self) -> None:
+            super().__init__()
+
+    class comp_opNode:
+        def __init__(self) -> None:
+            super().__init__()
+
+    class comp_eqNode:
+        def __init__(self) -> None:
+            super().__init__()
+
+    class log_opNode:
+        def __init__(self) -> None:
+            super().__init__()
+
 class AST:
     def __init__(self, rootNode, inputString) -> None:
-        super().__init__()
         self.input = inputString
         self.root = rootNode
         self.nodes = []
 
-    def insert(self, treeNode, substring):
-        self.nodes.insert(AST(treeNode, substring))
+    def insert(self, Node, substring):
+        self.nodes.insert(AST(Node, substring))
