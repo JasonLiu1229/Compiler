857706e75b1a703ea83b444d97c54647abda675b
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-03-17 17:51:52 +0100

    LLVM shit idk

--- src/LLVM.py
+++ src/LLVM.py
@@ -4,6 +4,7 @@ class LLVM:
 
     # TODO: var_node
     # TODO: AST
+
     def __init__(self, input_ast: AST = None, file_name: str = "Output.ll") -> None:
         """
         :type input_ast: object AST, default value None
@@ -12,15 +13,19 @@ class LLVM:
         self.ast = input_ast
         self.file_name = file_name
 
-    def var_node_convert(self, node: VarNode, scope_toggle: bool):
+    def var_node_convert(self, node : VarNode, scope_toggle: bool):
         with open(self.file_name, 'a') as f:
             ll_string = ""
             if scope_toggle:  # true global @
-                ll_string = "@" + node.key + " = "
+                ll_string = "@" + str(node.key) + " = "
             else:  # false local %
-                ll_string = "%" + node.key + " = alloca "
+                ll_string = "%" + str(node.key) + " = "
             if node.const:
                 ll_string += "constant "
+            elif scope_toggle:
+                ll_string += "global"
+            elif not scope_toggle:
+                ll_string += "alloca"
             if node.type == "int":
                 ll_string += "i32"
             elif node.type == "char":
@@ -31,7 +36,7 @@ class LLVM:
                 ll_string += "*" * node.total_deref
             else:
                 ll_string += " "
-            ll_string += node.value
+            ll_string += node.value + "\n"
             f.write(ll_string)
 
     def ast_convert(self, ast: AST):
@@ -41,7 +46,15 @@ class LLVM:
         pass
 
     def convert(self):
-        pass
+        # begin of main function
+        with open(self.file_name, 'a') as f:
+            f.write("define i32 @main #0 {")
+
+        # midd of main function
+
+        # end of main
+        with open(self.file_name, 'a') as f:
+            f.write("}")
 
     def execute(self):
         pass
