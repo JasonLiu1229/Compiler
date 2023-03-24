41076a13d165ecb2666a8d22567e9dddc774ba95
Orfeo Terkuci <terkuciorfeo@gmail.com>
2023-03-24 14:04:53 +0100

    code generation for variable declarations

--- src/LLVM.py
+++ src/LLVM.py
@@ -1,42 +1,70 @@
-from AST import *
+from array import array
 
+import numpy
+
+from AST import *
+from struct import *
+from decimal import *
+import numpy as np
 class LLVM:
 
     # TODO: var_node
     # TODO: AST
 
-    def __init__(self, input_ast: AST = None, file_name: str = "Output.ll") -> None:
+    def __init__(self, input_ast: AST = None, symbol_table = None , file_name: str = "../Output/Output.ll") -> None:
         """
         :type input_ast: object AST, default value None
         """
         super().__init__()
         self.ast = input_ast
+        self.symbol_table = symbol_table
         self.file_name = file_name
 
-    def var_node_convert(self, node : VarNode, scope_toggle: bool):
+    def var_node_convert(self, node: VarNode, scope_toggle: bool):
         with open(self.file_name, 'a') as f:
-            ll_string = ""
+            var_type = ""
+            ll_string = "\t"
             if scope_toggle:  # true global @
-                ll_string = "@" + str(node.key) + " = "
+                ll_string += "@" + str(node.key) + " = "
             else:  # false local %
-                ll_string = "%" + str(node.key) + " = "
+                ll_string += "%" + str(node.key) + " = "
+            # const or global
             if node.const:
                 ll_string += "constant "
             elif scope_toggle:
-                ll_string += "global"
+                ll_string += "global "
             elif not scope_toggle:
-                ll_string += "alloca"
+                ll_string += "alloca "
+            # get type
             if node.type == "int":
-                ll_string += "i32"
+                var_type = "i32"
             elif node.type == "char":
-                ll_string += "i8"
+                var_type = "i8"
             elif node.type == "float":
-                ll_string += "float"
+                var_type = "float"     # We had problems getting the exact decimal representation of our floats, so we're using doubles now
+            ll_string += var_type
             if node.ptr:
                 ll_string += "*" * node.total_deref
             else:
                 ll_string += " "
-            ll_string += node.value + "\n"
+            # allocate the value
+            if not scope_toggle:
+                ll_string += "\n\t"
+            while isinstance(node.value , Node):
+                ll_string += node.get_str() + "\n"
+            else:
+                # match type
+                if not scope_toggle and not node.const:
+                    ll_string += "store " + var_type + " "
+                    if isinstance(node.value , float):
+                        val = array('f' , [node.value])
+                        ll_string += str(val[0])
+                    else:
+                        ll_string += str(node.value)
+                    ll_string += "," + var_type + "* %" + str(node.key) + "\n"
+                else:
+                    ll_string += str(node.value) + "\n"
+
             f.write(ll_string)
 
     def ast_convert(self, ast: AST):
@@ -46,15 +74,17 @@ class LLVM:
         pass
 
     def convert(self):
+        # clear file
+        open(self.file_name, 'w')
         # begin of main function
         with open(self.file_name, 'a') as f:
-            f.write("define i32 @main #0 {")
-
-        # midd of main function
+            f.write("define i32 @main () {\n")
+        for node in self.symbol_table.values():
+            self.var_node_convert(node, False)
 
         # end of main
         with open(self.file_name, 'a') as f:
-            f.write("}")
+            f.write("\tret i32 0\n}")
 
     def execute(self):
         pass
