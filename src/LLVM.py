c6bc1b7ddd862030a9afcec44f974325a7e27a18
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-03-27 18:40:11 +0200

    printf integers changed

--- src/LLVM.py
+++ src/LLVM.py
@@ -96,7 +96,9 @@ class LLVM:
         out += "store i32" + str(value) + ", ptr %" + str(index) + "allign 4 \n"
         out += '%' + str(index + 1) + "= load i32, ptr %" + str(index) + ", allign 4 \n"
         index += 1
-        out += '%' + str(index + 1) + "= call i32 (ptr, ...) @printf(ptr noundef @.str, i32 noundef %" + str(index) + '\n'
+        out += '%' + str(index + 1) \
+               + "= call i32 (ptr, ...) @printf(ptr noundef @.str, i32 noundef %" \
+               + str(index) + '\n'
         index += 1
         return out, index
 
