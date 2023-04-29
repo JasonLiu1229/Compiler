2fcc88e29b43bb7d404625dd453dc7e534fb1ef1
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-04-29 12:57:31 +0200

    if condition done

--- input_files/not_yet_implemented/Project4.c
+++ input_files/not_yet_implemented/Project4.c
@@ -3,14 +3,14 @@
 */
 { // unnamed scope
 //    int x = 1;
+    int i = 2;
+    int x = 1;
     {
-    3;
-    int i;
-    4;
+    if (i > 1 && i != 3){
+        x = 2;
     }
-    5;
-}
-6;
+    }
+    }
 //    int y = 2;
 //    int sjdnc = 2;
 //    int yqsdsqk = 2;
