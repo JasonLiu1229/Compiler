6cb6f3913aa2ec05a111a48624d99fe4f34d6d2f
Orfeo Terkuci <terkuciorfeo@gmail.com>
2023-05-02 18:27:14 +0200

    fixed init

--- src/AstCreator.py
+++ src/AstCreator.py
@@ -539,7 +539,7 @@ class AstCreator(MathVisitor):
                 updates_queue.append(new_entry)
                 old_parent = ast.parent
                 ast.parent = ast.parent.parent
-                ast.parent.children.insert(ast.parent.children.index(old_parent), ast)
+                ast.parent.children.insert(ast.parent.children.index(old_parent), ast.children[0])
                 node = InstrAST(Node("instr", None), [new_entry])
             elif isinstance(ast, DeclrAST):
                 if len(ast.children) != 1 or not isinstance(ast.children[0], VarNode):
