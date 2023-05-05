35497977970c124f5518387208c51c39ad2939b9
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-05-05 18:17:23 +0200

    Dot is done easy claps

---
+++ src/Dot.py
@@ -0,0 +1,29 @@
+from AST import *
+
+
+class dot:
+    def __init__(self, in_ast: AST) -> None:
+        self.dot = {}
+        self.ast: AST = in_ast
+
+    def connect(self):
+        visited = []
+        not_visited = [self.ast]
+        while len(not_visited) > 0:
+            current = not_visited.pop()
+            visited.append(current)
+            if isinstance(current, AST):
+                for child in current.children:
+                    if child not in visited and child not in not_visited:
+                        not_visited.append(child)
+                    if child not in self.dot:
+                        self.dot[child] = []
+                    self.dot[child].append(current)
+
+        # write as dot file
+        with open("ast.dot", "w") as f:
+            f.write("digraph {\n")
+            for key in self.dot:
+                for value in self.dot[key]:
+                    f.write(f"\"{key.root.key}\" -> \"{value.root.key}\"\n")
+            f.write("}")
