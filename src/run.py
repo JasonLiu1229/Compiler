ecb6efcaaef11989e75d581c823ecf2b9b99d354
Orfeo Terkuci <terkuciorfeo@gmail.com>
2023-05-05 14:32:18 +0200

    enabled llvm code generation

--- src/run.py
+++ src/run.py
@@ -27,12 +27,12 @@ def run(directory: str, file_type: str, filenames: list, verbose: bool = False, 
             ast = visitor.visit(parse_tree)
             # handle tree
             ast = visitor.resolve(ast)
-            ast.print(4, True, filename)
+            ast.print(4, False, filename)
             ast.symbolTable.print()
             visitor.warn()
             # ast.dot_language(filename, visitor.symbol_table)
-            # generator = LLVM(ast, visitor.symbol_table, "../Output/" + filename + ".ll")
-            # generator.convert()
+            generator = LLVM(ast,  "../Output/" + filename + ".ll")
+            generator.convert()
             # generator.execute()
             print(">>> Finished execution with exit code 0\n")
         except Exception as e:
@@ -43,7 +43,7 @@ def run(directory: str, file_type: str, filenames: list, verbose: bool = False, 
 def main():
     parser = argparse.ArgumentParser(prog="Compiler", description="Compiles the C files")
     parser.add_argument('-d', '--directory', help='directory of the file that needs to be parsed', required=True)
-    parser.add_argument('-t', '--fType', help='file type that needs to be checked', required=True)
+    parser.add_argument('-t', '--type', help='file type that needs to be checked', required=True)
     parser.add_argument('-a', '--all', action='store_true', help='this flag defines that all files will be checked')
     parser.add_argument('-f', '--files', nargs='+', help='this flag will define which specific files we want to test')
     parser.add_argument('-i', '--index', help='index of which file it is in the directory')
@@ -52,16 +52,16 @@ def main():
         args = parser.parse_args()
         filenames = []
         if args.files is not None:
-            run(directory=args.directory, file_type=args.fType, filenames=args.files)
+            run(directory=args.directory, file_type=args.type, filenames=args.files)
         elif args.index is not None:
             files = os.listdir(args.directory)
-            files_one = [files[int(args.index) - 1][:len(files[int(args.index) - 1]) - len(args.fType)]]
-            run(directory=args.directory, file_type=args.fType, filenames=files_one)
+            files_one = [files[int(args.index) - 1][:len(files[int(args.index) - 1]) - len(args.type)]]
+            run(directory=args.directory, file_type=args.type, filenames=files_one)
         else:
             for file in os.listdir(args.directory):
-                if file.endswith(args.fType):
-                    filenames.append(file[:len(file) - len(args.fType)])
-            run(directory=args.directory, file_type=args.fType, filenames=filenames)
+                if file.endswith(args.type):
+                    filenames.append(file[:len(file) - len(args.type)])
+            run(directory=args.directory, file_type=args.type, filenames=filenames)
     except Exception as e:
         print(f'Excepted with error \"{e}\"')
 
