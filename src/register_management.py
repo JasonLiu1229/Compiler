e1db7516649e16147a382c4182dd1ad183e7a171
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-05-16 17:59:18 +0200

    dab

--- src/register_management.py
+++ src/register_management.py
@@ -129,7 +129,7 @@ class argumentManager(Manager):
 
     def __init__(self) -> None:
         super().__init__(4)
-        list_objects = [self.add(Register(in_name="a" + str(i))) for i in range(4)]
+        list_objects = [Register(in_name="t" + str(i)) for i in range(0, 4)]
         self.head = list_objects[0]
         self.tail = list_objects[-1]
         for i in range(3):
@@ -184,7 +184,7 @@ class argumentManager(Manager):
             self.tail = tempHead
 
     def clear(self):
-        list_objects = [self.add(Register(in_name="a" + str(i))) for i in range(4)]
+        list_objects = [Register(in_name="t" + str(i)) for i in range(0, 4)]
         self.head = list_objects[0]
         self.tail = list_objects[-1]
         for i in range(3):
