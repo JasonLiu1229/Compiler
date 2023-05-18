592825e1bf9a11d586a513fd6982835e0c18cddf
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2023-05-18 14:56:10 +0200

    switch test

---
+++ input_files/alle_projecten/CorrectCode/not_working/Switch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int a = 0;
+    while (1){
+        switch (a) {
+            case 0:
+                printf("a = 0\n");
+                a++;
+                break;
+            case 1:
+                printf("a = 1\n");
+                a++;
+                break;
+            case 2:
+                a++;
+                break;
+            case 3:
+                printf("Something went wrong\n");
+                a++;
+                break;
+            default:
+                printf("a = %d\n", a);
+                a++;
+                break;
+        }
+        if (a == 3){
+            break;
+        }
+    }
+    return 0;
+}
