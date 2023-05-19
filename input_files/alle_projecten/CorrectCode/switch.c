837a204d14d756791646e75d132b8c7b41f5f347
Orfeo Terkuci <terkuciorfeo@gmail.com>
2023-05-19 11:51:00 +0200

    added test for switch

---
+++ input_files/alle_projecten/CorrectCode/switch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    switch (day) {
+        case 6:
+            printf("Today is Saturday");
+            break;
+        case 7:
+            printf("Today is Sunday");
+            break;
+        default:
+            printf("Looking forward to the Weekend");
+    }
+}
