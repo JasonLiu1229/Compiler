cbcd1d795f1eb78f6ff08b824c20852886656364
Orfeo Terkuci <terkuciorfeo@gmail.com>
2023-06-02 17:28:34 +0200

    new comments test

--- input_files/CorrectCode/fully_working/comments.c
+++ input_files/CorrectCode/fully_working/comments.c
@@ -2,8 +2,9 @@
 int main(){
     // single line comment in function
     printf("Hello World");
-    /* multi-line comment in function
-        line 2
-        line 3
+    /*
+    multi-line comment in function
+    line 2
+    line 3
     */
 }
